240aa42d80b1ba2b226184836d1ebeca4b2611e6
Ashley Scripka <ashley.scripka@gmail.com>
2015-06-24 12:43:04 -0400

    Updates to allow installation from a directory

--- opal.sh
+++ opal.sh
@@ -3,19 +3,27 @@ if [[ "$?" = 1 ]]; then
 	sudo apt-get install jq
 fi
 
-OPALSERVER=130.207.211.77/opalserver/api/0.1/
-OPALS=$(sudo curl --silent $OPALSERVER/opals/)
 
 if [ -z "$BEDROCK_DIR" ]; then
 	BEDROCK_DIR=~/bedrock/
 fi 
 
+OPALSERVER=130.207.211.77/opalserver/api/0.1/ #sets OPALSERVER to the default server
+OPALS=$(sudo curl --silent $OPALSERVER/opals/) #sets OPALS to the contents of master_confug.json
+REMOTE=true #REMOTE true means that these are not local  
+if [ -f "$BEDROCK_DIR$2/config.json" ]; then #if the path has a config.json file it means that this is not part of opalserver already
+	OPALS=$(cat $BEDROCK_DIR$2/config.json) #set OPALS to the content of that config.json file
+	REMOTE=false #path is local so remote is set to false
+fi 
+
+
 if [ $1 = "-h" ]; then
-	echo "Use this script to install, remove, or reload opals or to install and remove applications:"
+	echo "Use this script to install, remove, reload, or validate opals or to install and remove applications:"
 	echo ""
 	echo "    opal install [name of opal to be installed]"
 	echo "    opal remove [name of opal to be removed]"
 	echo "    opal reload [name of opal to be reloaded]"
+	echo "		opal validate [name of opal to be validated]"
 	echo ""
 	echo "    opal install application [filepath for application json specification]"
 	echo "    opal remove application [filepath for application json specification]"
@@ -93,30 +101,58 @@ elif [ $1 = "list" ]; then
 
 fi
 
-
-HOST=$(echo $OPALS | jq '.["'$2'"]'.host)
-HOST="${HOST%\"}"
-HOST="${HOST#\"}"
-REPO=$(echo $OPALS | jq '.["'$2'"]'.repo)
-REPO="${REPO%\"}"
-REPO="${REPO#\"}"
-
-SUPPORTS=$(echo $OPALS | jq '.["'$2'"]'.supports)
-UNITS=$(echo $OPALS | jq '.["'$2'"]'.units)
-API=$(echo $OPALS | jq '.["'$2'"]'.api)
-API="${API%\"}"
-API="${API#\"}"
-INTERFACE=$(echo $OPALS | jq '.["'$2'"]'.interface)
-INTERFACE="${INTERFACE%\"}"
-INTERFACE="${INTERFACE#\"}"
-
-SCRIPT=$(echo $OPALS | jq '.["'$2'"]'.installation_script)
-
-SYSTEM=$(echo $OPALS | jq '.["'$2'"]'.system_dependencies)
-
-TARGET=/var/www/bedrock/$API/opals/
+count=0
+input=""
+if [ $1 = "validate" ]; then 
+	for arg in "$@" #move through each argument entered into command line
+	do
+		count=$((count + 1)) #automatically increment the count by 1
+		if [ "$arg" = "--filename" ]; then 
+			inputNumber=$((count + 1)) #the arguement after --filepath will be the filepath so increase count by 1 and set to inputNumber
+		fi
+		if [ "$inputNumber" = "$count" ]; then
+			filename="$arg"
+		fi
+	done
+	input=$(echo "$filename" | rev | cut -d/ -f2 | rev) #parse the filename till it only leaves opal-[]-[]
+else
+	input="$2" #input is whatever the normal input would be if not trying to validate
+fi
 
 
+VALID_CONF=true
+data=$(echo $OPALS | jq '.["'$input'"]') #data is set to the value of the config.json file
+if [ $data = "null" ]; then
+	VALID_CONF=false
+fi
+echo $data
+
+if [ "$VALID_CONF" = true ]; then 
+	HOST=$(echo $OPALS | jq '.["'$input'"]'.host)
+	HOST="${HOST%\"}"
+	HOST="${HOST#\"}"
+	REPO=$(echo $OPALS | jq '.["'$input'"]'.repo)
+	REPO="${REPO%\"}"
+	REPO="${REPO#\"}"
+
+	SUPPORTS=$(echo $OPALS | jq '.["'$input'"]'.supports)
+	UNITS=$(echo $OPALS | jq '.["'$input'"]'.units)
+	API=$(echo $OPALS | jq '.["'$input'"]'.api)
+	API="${API%\"}"
+	API="${API#\"}"
+	INTERFACE=$(echo $OPALS | jq '.["'$input'"]'.interface)
+	INTERFACE="${INTERFACE%\"}"
+	INTERFACE="${INTERFACE#\"}"
+
+	SCRIPT=$(echo $OPALS | jq '.["'$input'"]'.installation_script)
+
+	SYSTEM=$(echo $OPALS | jq '.["'$input'"]'.system_dependencies)
+
+	TARGET=/var/www/bedrock/$API/opals/
+else
+	echo "ERROR: File does not have a valid configuration."
+	exit 0
+fi
 
 if [ $1 = "install" ]; then
 
@@ -134,17 +170,28 @@ if [ $1 = "install" ]; then
 		done
 
 	else
-
+		if [ $2 = "-h" ]; then 
+			echo "Two Choices:"
+			echo "1: opal install [opal-name]"
+			echo "	*This opal must already be installed on the system."
+			echo ""
+			echo "2. opal install [full filepath to directory]"
+			echo "	*This dir must contain a conf.json file with metadata for opal."
+			exit 0
+		fi
 		if [ "$URL" = null ]; then
 			echo "ERROR: No opal by that name."
 			exit 0
-		fi 
+		fi
+
 		echo "Installing $2..."
-		if [ ! -d "$BEDROCK_DIR/$2" ]; then
-			git clone -b master --single-branch git@$HOST:$REPO $BEDROCK_DIR$2
-			if [ $? -ne 0 ]  # revert to HTTPS if keys not present
-			then
-			  git clone -b master --single-branch https://$HOST/$REPO $BEDROCK_DIR$2
+		if [ "$REMOTE" = true ]; then
+			if [ ! -d "$BEDROCK_DIR/$2" ]; then
+				git clone -b master --single-branch git@$HOST:$REPO $BEDROCK_DIR$2
+				if [ $? -ne 0 ]  # revert to HTTPS if keys not present
+				then
+			  	git clone -b master --single-branch https://$HOST/$REPO $BEDROCK_DIR$2
+				fi
 			fi
 		fi
 
@@ -289,7 +336,25 @@ elif [ $1 = "reload" ]; then
 	  python configure.py --mode reload --api $INTERFACE --filename $FILE
 	done	
 
+elif [ $1 = "validate" ]; then
+	if [ "$#" -ne 5 ]; then #there must be 5 arguments following opal.sh
+		echo "ERROR: validate must take exactly two arguments: "
+		echo "		--filename [absolute path for the location of the file]"
+		echo "		--input_directory [absoulte path for location of input files]"
+		exit 0
+	else
+		output_directory="/home/vagrant/bedrock/bedrock-core/validation/OutputStorage/" #the output directory will always be the same because whatever is made gets deleted right away
+		api="$INTERFACE" #using code above able to find the interface without the user having to input it
+		if [ $2 = "--filename" ]; then #scenario where --filename is the second argument
+			filename="$( cut -d ' ' -f 3 <<< "$@")" #the actual filename will follow the --filename so the third agrument is made filename
+			input_directory="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above
+		elif [ $2 = "--input_directory" ]; then #scenario where --input_directory is the second argument
+			input_directory="$( cut -d ' ' -f 3 <<< "$@")" #the actual input_directory will follow the --input_directory so the third agrument is made input_directory
+			filename="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above 
+		fi
+		python /home/vagrant/bedrock/bedrock-core/validation/validationScript.py --api "$api" --filename "$filename" --input_directory "$input_directory" --output_directory "$output_directory"
+		#call to validationScript.py that uses the inputs figured out above
+	fi
 else
     echo "Sorry, there is no script for that option"
 fi
-
