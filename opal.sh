7fa93e5d9fab63c40ca9c4637d51fe58cfb775ad
Ashley Scripka <ashley.scripka@gmail.com>
2015-09-17 18:17:05 -0400

    ensure script is run as root or sudo

--- opal.sh
+++ opal.sh
@@ -1,10 +1,19 @@
+if ! [ $(id -u) = 0 ]; then 
+    echo "Please start the script as root or sudo!" 
+    exit 1 
+fi
+
 dpkg-query -l jq | echo
-if [[ "$?" = 1 ]]; then
-    sudo apt-get install jq
+
+if [ "$?" = 1 ]; then
+    apt-get install jq
 fi
 
 if [ -z "$BEDROCK_DIR" ]; then
+    # BEDROCK_DIR=/home/vagrant/bedro
     BEDROCK_DIR=~/bedrock/
+    # echo "test" 
+    # echo $(BEDROCK_DIR)
 fi 
 
 if [ -z $1 ]; then
@@ -21,7 +30,7 @@ if [ -z $1 ]; then
     exit 0
 fi
 
-if [[ $1 = "-h" ]]; then
+if [ $1 = "-h" ]; then
     echo "Use this script to install, remove, reload, or validate opals or to install and remove applications:"
     echo ""
     echo "    opal install [name of opal to be installed]"
@@ -34,27 +43,27 @@ if [[ $1 = "-h" ]]; then
     echo ""
     exit 0
 
-elif [[ $1 = "reset" ]]; then
+elif [ $1 = "reset" ]; then
         if [ -z $2 ]; then
             echo "Resetting bedrock..."
 
-        sudo rm analytics/opals/*.py* 2>/dev/null
-        sudo rm analytics/*.pyc 2>/dev/null
+        rm analytics/opals/*.py* 2>/dev/null
+        rm analytics/*.pyc 2>/dev/null
         touch analytics/opals/__init__.py
 
 
-        sudo rm dataloader/opals/*.py* 2>/dev/null
-        sudo rm dataloader/*.pyc 2>/dev/null
+        rm dataloader/opals/*.py* 2>/dev/null
+        rm dataloader/*.pyc 2>/dev/null
         touch dataloader/opals/__init__.py
 
-        sudo rm visualization/opals/*.py* 2>/dev/null
-        sudo rm visualization/*.pyc 2>/dev/null
+        rm visualization/opals/*.py* 2>/dev/null
+        rm visualization/*.pyc 2>/dev/null
         touch visualization/opals/__init__.py
 
-        sudo rm *.pyc 2>/dev/null
+        rm *.pyc 2>/dev/null
 
-        sudo rm -r analytics/data/*
-        sudo rm -r dataloader/data/*
+        rm -r analytics/data/*
+        rm -r dataloader/data/*
 
         truncate -s 0 installed_opals.txt
 
@@ -64,35 +73,35 @@ elif [[ $1 = "reset" ]]; then
         fi
         exit 0
 
-elif [[ $1 = "clean" ]]; then
+elif [ $1 = "clean" ]; then
     if [ -z $2 ]; then
         echo "Cleaning bedrock..."
 
-        sudo rm analytics/opals/*.pyc 2>/dev/null
-        sudo rm analytics/*.pyc 2>/dev/null
+        rm analytics/opals/*.pyc 2>/dev/null
+        rm analytics/*.pyc 2>/dev/null
         touch analytics/opals/__init__.py
 
-        sudo rm dataloader/opals/*.pyc 2>/dev/null
-        sudo rm dataloader/*.pyc 2>/dev/null
+        rm dataloader/opals/*.pyc 2>/dev/null
+        rm dataloader/*.pyc 2>/dev/null
         touch dataloader/opals/__init__.py
 
-        sudo rm visualization/opals/*.pyc 2>/dev/null
-        sudo rm visualization/*.pyc 2>/dev/null
+        rm visualization/opals/*.pyc 2>/dev/null
+        rm visualization/*.pyc 2>/dev/null
         touch visualization/opals/__init__.py
 
-        sudo rm *.pyc 2>/dev/null
+        rm *.pyc 2>/dev/null
 
 
 
     else
         echo "Cleaning $2..."
-        sudo rm $2/opals/*.pyc 2>/dev/null
-        sudo rm $2/*.pyc 2>/dev/null
+        rm $2/opals/*.pyc 2>/dev/null
+        rm $2/*.pyc 2>/dev/null
 
     fi
     exit 0
 
-elif [[ $1 = "list" ]]; then
+elif [ $1 = "list" ]; then
 
     if [ $2 = "installed" ]; then
         cat installed_opals.txt
@@ -124,14 +133,14 @@ fi
 # fi
 
 
-if ! [[ $2 = "application" ]]; then
+if ! [ $2 = "application" ]; then
 
     #see if the directory is there
     #if not, ping the server for metadata and pull down the repo
     if [ ! -d "$BEDROCK_DIR/$2" ]; then
 
         OPALSERVER=130.207.211.77/opalserver/api/0.1/ #sets OPALSERVER to the default server
-        OPALS=$(sudo curl --silent $OPALSERVER/opals/) #sets OPALS to the contents of master_confug.json
+        OPALS=$(curl --silent $OPALSERVER/opals/) #sets OPALS to the contents of master_confug.json
         data=$(echo $OPALS | jq '.["'$2'"]') #data is set to the value of the config.json file
         if [[ $data = "null" ]]; then
             echo "ERROR: No opal with that name"
@@ -170,7 +179,7 @@ if ! [[ $2 = "application" ]]; then
     TARGET=/var/www/bedrock/$API/opals/
 fi
 
-if [[ $1 = "install" ]]; then
+if [ $1 = "install" ]; then
 
     if [ $2 = "application" ]; then
 
@@ -204,7 +213,7 @@ if [[ $1 = "install" ]]; then
         dpkg-query -l $f | echo
         if [[ "$?" = 1 ]]; then
             echo "Installing $f..."
-            sudo apt-get install $f
+            apt-get install $f
         fi
       fi
     done    
@@ -233,7 +242,7 @@ if [[ $1 = "install" ]]; then
         MY_PATH=$(readlink -f $f)
         if [ ! -L $TARGET$FILE ]; then
             echo "    linking: $MY_PATH"
-            sudo ln -s $MY_PATH $TARGET
+            ln -s $MY_PATH $TARGET
         fi
       fi
     done    
@@ -249,7 +258,7 @@ if [[ $1 = "install" ]]; then
       FILE=$(basename $f)
       if [ ! -L $TARGET$FILE ]; then
           echo "    linking: $MY_PATH"
-          sudo ln -s $MY_PATH $TARGET
+          ln -s $MY_PATH $TARGET
       fi
       python configure.py --mode add --api $INTERFACE --filename $FILE
       fi
@@ -258,7 +267,7 @@ if [[ $1 = "install" ]]; then
     echo $2 >> installed_opals.txt
 
 
-elif [[ $1 = "remove" ]]; then
+elif [ $1 = "remove" ]; then
     if [ $2 = "application" ]; then
 
         echo "Removing application $3..."
@@ -286,7 +295,7 @@ elif [[ $1 = "remove" ]]; then
               f=$BEDROCK_DIR/$2/$f
               FILE=$(basename $f)
               echo "    unlinking: $FILE"
-              sudo rm $TARGET$FILE
+              rm $TARGET$FILE
           fi
         done    
 
@@ -300,16 +309,16 @@ elif [[ $1 = "remove" ]]; then
               MY_PATH=$(readlink -f $f)
               FILE=$(basename $f)
               echo "    unlinking: $MY_PATH"
-              sudo rm $TARGET$FILE
+              rm $TARGET$FILE
               python configure.py --mode remove --api $INTERFACE --filename $FILE
           fi
         done
 
-        sudo sed -e /$2/d -i installed_opals.txt
+        sed -e /$2/d -i installed_opals.txt
 
     fi
 
-elif [[ $1 = "reload" ]]; then
+elif [ $1 = "reload" ]; then
     echo "Reloading $2..."
 
     #iterate through the units and symlink/install
@@ -321,30 +330,30 @@ elif [[ $1 = "reload" ]]; then
       FILE=$(basename $f)
       python configure.py --mode reload --api $INTERFACE --filename $FILE
     done    
-    sudo service apache2 reload
+    service apache2 reload
 
-elif [[ $1 = "validate" ]]; then
-    if [ "$#" -ne 7 ]; then #there must be 5 arguments following opal.sh
-        echo "ERROR: validate must take exactly three arguments: "
-        echo "      --filename [absolute path for the location of the file]"
-        echo "      --input_directory [absoulte path for location of input files]"
-        echo "      --filter_specs ['{filters}'] or N/A if no filters"
-        exit 0
-    else
-        output_directory="/home/vagrant/bedrock/bedrock-core/validation/OutputStorage/" #the output directory will always be the same because whatever is made gets deleted right away
-        api="$INTERFACE" #using code above able to find the interface without the user having to input it
+# elif [ $1 = "validate" ]; then
+#     if [ "$#" -ne 7 ]; then #there must be 5 arguments following opal.sh
+#         echo "ERROR: validate must take exactly three arguments: "
+#         echo "      --filename [absolute path for the location of the file]"
+#         echo "      --input_directory [absoulte path for location of input files]"
+#         echo "      --filter_specs ['{filters}'] or N/A if no filters"
+#         exit 0
+#     else
+#         output_directory="/home/vagrant/bedrock/bedrock-core/validation/OutputStorage/" #the output directory will always be the same because whatever is made gets deleted right away
+#         api="$INTERFACE" #using code above able to find the interface without the user having to input it
         
-        if [ $2 = "--filename" ]; then #scenario where --filename is the second argument
-            filename="$( cut -d ' ' -f 3 <<< "$@")" #the actual filename will follow the --filename so the third agrument is made filename
-            input_directory="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above
-        elif [ $2 = "--input_directory" ]; then #scenario where --input_directory is the second argument
-            input_directory="$( cut -d ' ' -f 3 <<< "$@")" #the actual input_directory will follow the --input_directory so the third agrument is made input_directory
-            filename="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above 
-        fi
-        #call to validationScript.py that uses the inputs figured out above
-        filter_specs="$7"
-        python /home/vagrant/bedrock/bedrock-core/validation/validationScript.py --api "$api" --filename "$filename" --input_directory "$input_directory" --filter_specs "$filter_specs" --output_directory "$output_directory"
-    fi
+#         if [ $2 = "--filename" ]; then #scenario where --filename is the second argument
+#             filename="$( cut -d ' ' -f 3 <<< "$@")" #the actual filename will follow the --filename so the third agrument is made filename
+#             input_directory="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above
+#         elif [ $2 = "--input_directory" ]; then #scenario where --input_directory is the second argument
+#             input_directory="$( cut -d ' ' -f 3 <<< "$@")" #the actual input_directory will follow the --input_directory so the third agrument is made input_directory
+#             filename="$( cut -d ' ' -f 5 <<< "$@")" #same reasoning as above 
+#         fi
+#         #call to validationScript.py that uses the inputs figured out above
+#         filter_specs="$7"
+#         python /home/vagrant/bedrock/bedrock-core/validation/validationScript.py --api "$api" --filename "$filename" --input_directory "$input_directory" --filter_specs "$filter_specs" --output_directory "$output_directory"
+#     fi
 else
     echo "Sorry, there is no script for that option"
 fi
