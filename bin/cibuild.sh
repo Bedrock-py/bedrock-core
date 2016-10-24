f5c05b3eda226f1dfe7626fc7d456bd197807121
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2016-10-24 16:36:44 -0400

    move tests to pytest introduce client library
    closes #31, #33
    works on py3k port #22
    fix path constructs per code review #41
    fix imports per code review #41
    fix RESUTLS_PATH to RESULTS_PATH

--- bin/cibuild.sh
+++ bin/cibuild.sh
@@ -18,14 +18,24 @@ ls src
 
 pip install -q -r requirements.txt
 export PATH="$(pwd)/bin:$PATH"
-echo "INFO: Downloading opals"
-pushd /var/www && curl --silent http://130.207.211.77/packages/opals.tar.gz | tar xz
-popd
+export PYTHONPATH="$(pwd):$PYTHON_PATH"
+python -c "import sys; print('PYTHONPATH: ' + str(sys.path))"
+python -c "import src.analytics; import src.client; import src.dataloader; import src.visualization"
+echo "INFO: can import bedrock src"
+
+if [ ! -r opals.tar.gz ]; then
+    echo "INFO: Downloading opals"
+    pushd /var/www && curl --silent http://130.207.211.77/packages/opals.tar.gz | tar xz
+    popd
+else
+    echo "INFO: opals already downloaded make sure they are installed too."
+fi
+
 echo "INFO: Installing with ./bin/install.sh"
 ./bin/install.sh
 echo "INFO: running setup.py with "
 python ./bin/setup.py
 echo "running tests"
-curl localhost:81
+curl localhost:81 > /dev/null
 python CONSTANTS.py
-python test/test.py
+pytest
