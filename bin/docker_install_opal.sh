78970a4125fe4f535151d541a097765bd523a7a7
Jason Poovey <japoovey@gmail.com>
2017-07-18 14:18:39 -0400

    Adding the ability to reload an opal in a docker container using the git url

--- bin/docker_install_opal.sh
+++ bin/docker_install_opal.sh
@@ -8,9 +8,10 @@
 
 display_usage() {
 	echo -e "\tdocker_install_opal.sh is used to install an opal in a running docker container"
-	echo -e "\tUsage: docker_install_opal.sh ID PACKAGE"
+	echo -e "\tUsage: docker_install_opal.sh ID LOCAL_PACKAGE\n\tOR\n\tdocker_install_opal.sh -g GIT_URL ID"
 	echo -e "\t\tID - ID of the running Bedrock Docker container"
 	echo -e "\t\tPACKAGE - Name of the package to run. \$PACKAGE.tar.gz or PACKAGE/ should exist in the current directory \n"
+	echo -e "\t\GIT_URL - URL to the git repo hosting the Opal (https only)\n"
 	}
 
 # if less than two arguments supplied, display usage
@@ -20,13 +21,23 @@ then
 	exit 1
 fi
 
-ID=$1
-PACKAGE=$2
+GIT_URL=''
 
+while getopts 'g:' flag; do
+  case "${flag}" in
+    g) GIT_URL="git+${OPTARG}" ;;
+  esac
+done
 
+ID=${@:$OPTIND:1}
+PACKAGE=${@:$OPTIND+1:1}
 
 docker exec $ID mkdir -p /opt/bedrock/opals/
-if [ -e $PACKAGE.tar.gz ]; then
+if [[ ! -z $GIT_URL ]]; then
+  docker exec $ID pip install $GIT_URL
+  docker exec $ID service apache2 reload
+  exit 0
+elif [ -e $PACKAGE.tar.gz ]; then
   docker cp $PACKAGE.tar.gz $ID:/opt/bedrock/opals/
   docker exec $ID tar -zxf /opt/bedrock/opals/$PACKAGE.tar.gz -C /opt/bedrock/opals
 elif [ -d $PACKAGE ]; then
